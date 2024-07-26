5635c078b79ede92a0306782ce72116e211391e1
marvinlanhenke <lanhenke@googlemail.com>
2024-07-26 08:35:22 +0200

    feat: impl 'ProjectionExecStream'

--- src/plan/physical/projection.rs
+++ src/plan/physical/projection.rs
@@ -1,6 +1,13 @@
-use std::{any::Any, fmt::Display, sync::Arc};
+use std::{
+    any::Any,
+    fmt::Display,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
 
-use arrow::datatypes::SchemaRef;
+use arrow::{array::RecordBatch, datatypes::SchemaRef};
+use futures::{Stream, StreamExt};
 
 use crate::{error::Result, expression::physical::expr::PhysicalExpression, io::RecordBatchStream};
 
@@ -41,9 +48,9 @@ impl ExecutionPlan for ProjectionExec {
     }
 
     fn execute(&self) -> Result<RecordBatchStream> {
-        let _input = self.input.execute()?;
-        // create a new stream with projected cols here
-        todo!()
+        let input = self.input.execute()?;
+        let stream = ProjectionExecStream::new(input, self.schema.clone(), self.expression.clone());
+        Ok(stream.boxed())
     }
 
     fn format(&self) -> String {
@@ -54,5 +61,40 @@ impl ExecutionPlan for ProjectionExec {
 impl Display for ProjectionExec {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         format_exec(self, f, 0)
+    }
+}
+
+struct ProjectionExecStream {
+    input: RecordBatchStream,
+    schema: SchemaRef,
+    expression: Vec<Arc<dyn PhysicalExpression>>,
+}
+
+impl ProjectionExecStream {
+    pub fn new(
+        input: RecordBatchStream,
+        schema: SchemaRef,
+        expression: Vec<Arc<dyn PhysicalExpression>>,
+    ) -> Self {
+        Self {
+            input,
+            schema,
+            expression,
+        }
+    }
+
+    fn project_batch(&self, batch: &RecordBatch) -> Result<RecordBatch> {
+        todo!()
+    }
+}
+
+impl Stream for ProjectionExecStream {
+    type Item = Result<RecordBatch>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.input.poll_next_unpin(cx).map(|opt| match opt {
+            Some(Ok(batch)) => Some(self.project_batch(&batch)),
+            other => other,
+        })
     }
 }
