fa6bcb63f2ed9a7b0bac0252f7bbf430e591b2c2
marvinlanhenke <lanhenke@googlemail.com>
2024-07-26 06:30:54 +0200

    chore: add stubs 'ColumnarValue' and 'ScalarValue'

---
+++ src/expression/scalar.rs
@@ -0,0 +1,42 @@
+use crate::error::Result;
+use arrow::array::ArrayRef;
+
+#[derive(Debug)]
+pub enum ColumnarValue {
+    Array(ArrayRef),
+    Scalar(ScalarValue),
+}
+
+impl ColumnarValue {
+    pub fn into_array(self, num_rows: usize) -> Result<ArrayRef> {
+        use ColumnarValue::*;
+
+        Ok(match self {
+            Array(v) => v,
+            Scalar(v) => v.to_array(num_rows),
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ScalarValue {
+    Null,
+    Boolean(Option<bool>),
+    Int8(Option<i8>),
+    Int16(Option<i16>),
+    Int32(Option<i32>),
+    Int64(Option<i64>),
+    UInt8(Option<u8>),
+    UInt16(Option<u16>),
+    UInt32(Option<u32>),
+    UInt64(Option<u64>),
+    Float32(Option<f32>),
+    Float64(Option<f64>),
+    Utf8(Option<String>),
+}
+
+impl ScalarValue {
+    pub fn to_array(&self, _num_rows: usize) -> ArrayRef {
+        todo!()
+    }
+}
