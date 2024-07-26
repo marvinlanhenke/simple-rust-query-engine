e701916b7b88f1417b36f0f5b72a763a1627b1cf
marvinlanhenke <lanhenke@googlemail.com>
2024-07-26 16:09:27 +0200

    feat: add basic 'Filter' logical plan

---
+++ src/plan/logical/filter.rs
@@ -0,0 +1,50 @@
+use std::fmt::Display;
+
+use arrow::datatypes::SchemaRef;
+
+use crate::{error::Result, expression::logical::expr::Expression};
+
+use super::plan::LogicalPlan;
+
+/// Represents a filter operation in a logical plan.
+#[derive(Debug)]
+pub struct Filter {
+    /// The input [`LogicalPlan`].
+    input: Box<LogicalPlan>,
+    /// The filter predicate to apply.
+    predicate: Expression,
+}
+
+impl Filter {
+    /// Attempts to create a new [`Filter`] instance.
+    pub fn try_new(input: Box<LogicalPlan>, predicate: Expression) -> Result<Self> {
+        // check if predicate evals to boolean
+        Ok(Self { input, predicate })
+    }
+
+    /// Retrieves the input [`LogicalPlan`].
+    pub fn input(&self) -> &LogicalPlan {
+        &self.input
+    }
+
+    /// Retrieves the filter predicate applied to [`Filter`].
+    pub fn predicate(&self) -> &Expression {
+        &self.predicate
+    }
+
+    /// A reference-counted [`arrow::datatypes::Schema`] of the input plan.
+    pub fn schema(&self) -> SchemaRef {
+        self.input.schema()
+    }
+
+    /// Retrieves the child logical plans.
+    pub fn children(&self) -> Vec<&LogicalPlan> {
+        vec![&self.input]
+    }
+}
+
+impl Display for Filter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Filter: [{}]", self.predicate)
+    }
+}
--- src/plan/logical/mod.rs
+++ src/plan/logical/mod.rs
@@ -1,3 +1,4 @@
+pub mod filter;
 pub mod plan;
 pub mod projection;
 pub mod scan;
