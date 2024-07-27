ca4f9c546e8149763e0843325aa2422968faacf2
marvinlanhenke <lanhenke@googlemail.com>
2024-07-27 17:33:04 +0200

    feat: impl eval for 'BinaryExpr'

--- src/expression/physical/binary.rs
+++ src/expression/physical/binary.rs
@@ -1,13 +1,27 @@
 use std::{any::Any, fmt::Display, sync::Arc};
 
 use arrow::{
-    array::RecordBatch,
+    array::{ArrayRef, BooleanArray, Datum, RecordBatch},
+    compute::{
+        and_kleene,
+        kernels::{
+            cmp::{eq, gt, gt_eq, lt, lt_eq, neq},
+            numeric::{add_wrapping, div, mul_wrapping, sub_wrapping},
+        },
+        or_kleene,
+    },
     datatypes::{DataType, Schema},
+    error::ArrowError,
 };
+use snafu::location;
 
 use crate::{
-    error::Result,
-    expression::{coercion::Signature, operator::Operator, values::ColumnarValue},
+    error::{Error, Result},
+    expression::{
+        coercion::Signature,
+        operator::Operator,
+        values::{ColumnarValue, ScalarValue},
+    },
 };
 
 use super::expr::PhysicalExpression;
@@ -27,6 +41,33 @@ impl BinaryExpr {
     ) -> Self {
         Self { lhs, op, rhs }
     }
+
+    fn apply_cmp(
+        lhs: &ColumnarValue,
+        rhs: &ColumnarValue,
+        f: impl Fn(&dyn Datum, &dyn Datum) -> std::result::Result<BooleanArray, ArrowError>,
+    ) -> Result<ColumnarValue> {
+        Self::apply(lhs, rhs, |l, r| Ok(Arc::new(f(l, r)?)))
+    }
+
+    fn apply(
+        lhs: &ColumnarValue,
+        rhs: &ColumnarValue,
+        f: impl Fn(&dyn Datum, &dyn Datum) -> std::result::Result<ArrayRef, ArrowError>,
+    ) -> Result<ColumnarValue> {
+        use ColumnarValue::*;
+
+        match (&lhs, &rhs) {
+            (Array(l), Array(r)) => Ok(Array(f(&l.as_ref(), &r.as_ref())?)),
+            (Scalar(l), Array(r)) => Ok(Array(f(&l.to_scalar()?, &r.as_ref())?)),
+            (Array(l), Scalar(r)) => Ok(Array(f(&l.as_ref(), &r.to_scalar()?)?)),
+            (Scalar(l), Scalar(r)) => {
+                let arr = f(&l.to_scalar()?, &r.to_scalar()?)?;
+                let value = ScalarValue::try_from_array(arr.as_ref(), 0)?;
+                Ok(Scalar(value))
+            }
+        }
+    }
 }
 
 impl PhysicalExpression for BinaryExpr {
@@ -40,8 +81,65 @@ impl PhysicalExpression for BinaryExpr {
         Signature::get_result_type(&lhs, &self.op, &rhs)
     }
 
-    fn eval(&self, _input: &RecordBatch) -> Result<ColumnarValue> {
-        todo!()
+    fn eval(&self, input: &RecordBatch) -> Result<ColumnarValue> {
+        use Operator::*;
+
+        let lhs = self.lhs.eval(input)?;
+        let rhs = self.rhs.eval(input)?;
+
+        match self.op {
+            Eq => Self::apply_cmp(&lhs, &rhs, eq),
+            NotEq => Self::apply_cmp(&lhs, &rhs, neq),
+            Lt => Self::apply_cmp(&lhs, &rhs, lt),
+            LtEq => Self::apply_cmp(&lhs, &rhs, lt_eq),
+            Gt => Self::apply_cmp(&lhs, &rhs, gt),
+            GtEq => Self::apply_cmp(&lhs, &rhs, gt_eq),
+            Plus => Self::apply(&lhs, &rhs, add_wrapping),
+            Minus => Self::apply(&lhs, &rhs, sub_wrapping),
+            Multiply => Self::apply(&lhs, &rhs, mul_wrapping),
+            Divide => Self::apply(&lhs, &rhs, div),
+            Or | And => {
+                let (left, right) = (
+                    lhs.into_array(input.num_rows())?,
+                    rhs.into_array(input.num_rows())?,
+                );
+
+                if left.data_type() != &DataType::Boolean {
+                    return Err(Error::InvalidOperation {
+                        message: format!(
+                            "Cannot evaluate binary expression {} with types {} and {}",
+                            self.op,
+                            left.data_type(),
+                            right.data_type()
+                        ),
+                        location: location!(),
+                    });
+                }
+
+                let left = left
+                    .as_any()
+                    .downcast_ref::<BooleanArray>()
+                    .ok_or_else(|| Error::Arrow {
+                        message: "Failed to downcast array".to_string(),
+                        location: location!(),
+                    })?;
+                let right = right
+                    .as_any()
+                    .downcast_ref::<BooleanArray>()
+                    .ok_or_else(|| Error::Arrow {
+                        message: "Failed to downcast array".to_string(),
+                        location: location!(),
+                    })?;
+
+                let array = if self.op == And {
+                    and_kleene(left, right)?
+                } else {
+                    or_kleene(left, right)?
+                };
+
+                Ok(ColumnarValue::Array(Arc::new(array)))
+            }
+        }
     }
 }
 
