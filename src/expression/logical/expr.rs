4f0c50312ebe7423e4e102ce1e9b23fccc41beea
marvinlanhenke <lanhenke@googlemail.com>
2024-07-28 06:07:05 +0200

    chore: add tests

--- src/expression/logical/expr.rs
+++ src/expression/logical/expr.rs
@@ -86,4 +86,60 @@ make_expr_fn!(and, And);
 make_expr_fn!(or, Or);
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::sync::Arc;
+
+    use arrow::datatypes::DataType;
+
+    use crate::{
+        expression::{
+            logical::{binary::Binary, column::Column},
+            operator::Operator,
+            values::ScalarValue,
+        },
+        io::reader::csv::{options::CsvReadOptions, source::CsvDataSource},
+        plan::logical::{plan::LogicalPlan, scan::Scan},
+        tests::create_schema,
+    };
+
+    use super::Expression;
+
+    #[test]
+    fn test_expr_to_field() {
+        let schema = Arc::new(create_schema());
+        let options = CsvReadOptions::builder().with_schema(Some(schema)).build();
+        let source = CsvDataSource::try_new("test_path", options).unwrap();
+        let plan = LogicalPlan::Scan(Scan::new("test_path", Arc::new(source), None, vec![]));
+
+        let cols = [
+            ("c1", DataType::Utf8, Expression::Column(Column::new("c1"))),
+            ("c2", DataType::Int64, Expression::Column(Column::new("c2"))),
+            ("c3", DataType::Int64, Expression::Column(Column::new("c3"))),
+        ];
+
+        for (name, data_type, col) in cols.iter() {
+            let field = col.to_field(&plan).unwrap();
+            assert_eq!(field.name(), name);
+            assert_eq!(field.data_type(), data_type);
+        }
+    }
+
+    #[test]
+    fn test_expr_data_type() {
+        let schema = create_schema();
+        let column = Arc::new(Expression::Column(Column::new("c1")));
+        let literal = Arc::new(Expression::Literal(ScalarValue::Utf8(Some(
+            "a".to_string(),
+        ))));
+        let binary = Expression::Binary(Binary::new(column.clone(), Operator::Eq, literal.clone()));
+
+        let result = column.data_type(&schema).unwrap();
+        assert_eq!(result, DataType::Utf8);
+
+        let result = literal.data_type(&schema).unwrap();
+        assert_eq!(result, DataType::Utf8);
+
+        let result = binary.data_type(&schema).unwrap();
+        assert_eq!(result, DataType::Boolean);
+    }
+}
