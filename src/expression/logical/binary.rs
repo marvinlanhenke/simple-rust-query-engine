35a9495579dfb83c31172534091aa267a03c4e53
marvinlanhenke <lanhenke@googlemail.com>
2024-07-28 05:51:41 +0200

    chore: add docs

--- src/expression/logical/binary.rs
+++ src/expression/logical/binary.rs
@@ -4,26 +4,34 @@ use crate::expression::operator::Operator;
 
 use super::expr::Expression;
 
+/// Represents a binary expression in the logical plan.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Binary {
+    /// The left [`Expression`].
     lhs: Arc<Expression>,
+    /// The [`Operator`] for the expression.
     op: Operator,
+    /// The right [`Expression`].
     rhs: Arc<Expression>,
 }
 
 impl Binary {
+    /// Creates a new [`Binary`] instance.
     pub fn new(lhs: Arc<Expression>, op: Operator, rhs: Arc<Expression>) -> Self {
         Self { lhs, op, rhs }
     }
 
+    /// A reference to the left [`Expression`].
     pub fn lhs(&self) -> &Expression {
         &self.lhs
     }
 
+    /// A reference to the [`Operator`].
     pub fn op(&self) -> &Operator {
         &self.op
     }
 
+    /// A reference to the right [`Expression`].
     pub fn rhs(&self) -> &Expression {
         &self.rhs
     }
