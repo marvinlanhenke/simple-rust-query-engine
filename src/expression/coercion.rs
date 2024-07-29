71948be5839e335f574c42f486cad3369542abcd
marvinlanhenke <lanhenke@googlemail.com>
2024-07-29 06:04:05 +0200

    chore: add docs + tests

--- src/expression/coercion.rs
+++ src/expression/coercion.rs
@@ -9,6 +9,8 @@ use crate::error::{Error, Result};
 
 use super::operator::Operator;
 
+/// Represents the signature of an operation,
+/// including the input and output data types.
 #[derive(Debug)]
 pub struct Signature {
     /// The left input's [`DataType`].
@@ -20,6 +22,7 @@ pub struct Signature {
 }
 
 impl Signature {
+    /// Attempts to create a new [`Signature`] instance.
     fn try_new(lhs: &DataType, op: &Operator, rhs: &DataType) -> Result<Self> {
         use DataType::*;
         use Operator::*;
@@ -34,6 +37,7 @@ impl Signature {
             }
         };
 
+        // no type coercion supported yet
         match op {
             Eq | NotEq | Lt | LtEq | Gt | GtEq => {
                 if lhs != rhs {
@@ -56,6 +60,9 @@ impl Signature {
                 })
             }
             Plus | Minus | Multiply | Divide => {
+                if lhs != rhs {
+                    return Err(coercion_err(lhs, op, rhs));
+                }
                 let left = new_empty_array(lhs);
                 let right = new_empty_array(rhs);
                 let result: ArrayRef = match op {
@@ -74,15 +81,82 @@ impl Signature {
         }
     }
 
+    /// Gets the result data type of an operation given the input data types and operator.
     pub fn get_result_type(lhs: &DataType, op: &Operator, rhs: &DataType) -> Result<DataType> {
         Self::try_new(lhs, op, rhs).map(|sig| sig.ret)
     }
 
+    /// Gets the input data type of an operation given the input data types and operator.
     pub fn get_input_types(
         lhs: &DataType,
         op: &Operator,
         rhs: &DataType,
     ) -> Result<(DataType, DataType)> {
         Self::try_new(lhs, op, rhs).map(|sig| (sig.lhs, sig.rhs))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use arrow::datatypes::DataType;
+
+    use crate::expression::operator::Operator;
+
+    use super::Signature;
+
+    #[test]
+    fn test_signature_coercion_err() {
+        let lhs = DataType::Int32;
+        let rhs = DataType::Int64;
+        let op = Operator::Eq;
+        let sig = Signature::try_new(&lhs, &op, &rhs);
+        let input_type = Signature::get_input_types(&lhs, &op, &rhs);
+        let result_type = Signature::get_result_type(&lhs, &op, &rhs);
+
+        assert!(sig.is_err());
+        assert!(input_type.is_err());
+        assert!(result_type.is_err());
+    }
+
+    #[test]
+    fn test_signature_numeric() {
+        let lhs = DataType::Int64;
+        let rhs = DataType::Int64;
+        let op = Operator::Plus;
+        let sig = Signature::try_new(&lhs, &op, &rhs);
+        let input_type = Signature::get_input_types(&lhs, &op, &rhs).unwrap();
+        let result_type = Signature::get_result_type(&lhs, &op, &rhs).unwrap();
+
+        assert!(sig.is_ok());
+        assert_eq!(input_type, (DataType::Int64, DataType::Int64));
+        assert_eq!(result_type, DataType::Int64);
+    }
+
+    #[test]
+    fn test_signature_bool() {
+        let lhs = DataType::Boolean;
+        let rhs = DataType::Boolean;
+        let op = Operator::And;
+        let sig = Signature::try_new(&lhs, &op, &rhs);
+        let input_type = Signature::get_input_types(&lhs, &op, &rhs).unwrap();
+        let result_type = Signature::get_result_type(&lhs, &op, &rhs).unwrap();
+
+        assert!(sig.is_ok());
+        assert_eq!(input_type, (DataType::Boolean, DataType::Boolean));
+        assert_eq!(result_type, DataType::Boolean);
+    }
+
+    #[test]
+    fn test_signature_cmp() {
+        let lhs = DataType::Int32;
+        let rhs = DataType::Int32;
+        let op = Operator::Eq;
+        let sig = Signature::try_new(&lhs, &op, &rhs);
+        let input_type = Signature::get_input_types(&lhs, &op, &rhs).unwrap();
+        let result_type = Signature::get_result_type(&lhs, &op, &rhs).unwrap();
+
+        assert!(sig.is_ok());
+        assert_eq!(input_type, (DataType::Int32, DataType::Int32));
+        assert_eq!(result_type, DataType::Boolean);
     }
 }
