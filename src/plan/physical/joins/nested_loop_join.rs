d14006de0cab68a1e119e1c4f3e331029a364315
marvinlanhenke <lanhenke@googlemail.com>
2024-08-19 13:24:20 +0200

    feat: impl NestedLoopJoin

--- src/plan/physical/joins/nested_loop_join.rs
+++ src/plan/physical/joins/nested_loop_join.rs
@@ -5,10 +5,13 @@ use std::{
     task::Poll,
 };
 
-use arrow::{array::BooleanBufferBuilder, compute::concat_batches};
-use arrow_array::RecordBatch;
+use arrow::{
+    array::{BooleanBufferBuilder, UInt32Builder, UInt64Builder},
+    compute::concat_batches,
+};
+use arrow_array::{RecordBatch, UInt32Array, UInt64Array};
 use arrow_schema::SchemaRef;
-use futures::{future::BoxFuture, FutureExt, Stream, StreamExt, TryStreamExt};
+use futures::{future::BoxFuture, ready, FutureExt, Stream, StreamExt, TryStreamExt};
 use snafu::location;
 
 use crate::{
@@ -17,13 +20,13 @@ use crate::{
     plan::{
         logical::join::JoinType,
         physical::{
-            joins::utils::{create_join_schema, is_valid_join},
+            joins::utils::{build_batch_from_indices, create_join_schema, is_valid_join, JoinSide},
             plan::{format_exec, ExecutionPlan},
         },
     },
 };
 
-use super::utils::{JoinColumnIndex, JoinFilter};
+use super::utils::{apply_join_filter, JoinColumnIndex, JoinFilter};
 
 type InnerTableFuture = BoxFuture<'static, Result<InnerTableData>>;
 
@@ -38,14 +41,6 @@ struct InnerTableData {
 impl InnerTableData {
     fn new(batch: RecordBatch, visited: BooleanBufferBuilder) -> Self {
         Self { batch, visited }
-    }
-
-    fn batch(&self) -> &RecordBatch {
-        &self.batch
-    }
-
-    fn visited(&self) -> &BooleanBufferBuilder {
-        &self.visited
     }
 }
 
@@ -159,8 +154,9 @@ impl ExecutionPlan for NestedLoopJoinExec {
             join_type: self.join_type,
             inner_table,
             outer_table,
+            outer_table_schema: self.rhs.schema(),
             column_indices: self.column_indices.clone(),
-            is_exhaused: false,
+            state: NestedLoopJoinStreamState::WaitBuildSide,
         };
 
         Ok(stream.boxed())
@@ -180,22 +176,214 @@ impl Display for NestedLoopJoinExec {
     }
 }
 
+enum StreamResultState<T> {
+    Ready(T),
+    Continue,
+}
+
+macro_rules! handle_stream_result {
+    ($e: expr) => {
+        match $e {
+            Ok(StreamResultState::Continue) => continue,
+            Ok(StreamResultState::Ready(res)) => Poll::Ready(Ok(res).transpose()),
+            Err(e) => Poll::Ready(Some(Err(e))),
+        }
+    };
+}
+
+enum NestedLoopJoinStreamState {
+    WaitBuildSide,
+    FetchProbeBatch,
+    ProcessProbeBatch(RecordBatch),
+    ExhaustedProbeSide,
+    Completed,
+}
+
+impl NestedLoopJoinStreamState {
+    fn try_as_process_probe_batch(&self) -> Result<&RecordBatch> {
+        match self {
+            NestedLoopJoinStreamState::ProcessProbeBatch(batch) => Ok(batch),
+            _ => Err(Error::InvalidOperation {
+                message: "Expected stream in processing state".to_string(),
+                location: location!(),
+            }),
+        }
+    }
+}
+
 struct NestedLoopJoinStream {
     schema: SchemaRef,
     filter: Option<JoinFilter>,
     join_type: JoinType,
     inner_table: InnerTableState,
     outer_table: RecordBatchStream,
+    outer_table_schema: SchemaRef,
     column_indices: Vec<JoinColumnIndex>,
-    is_exhaused: bool,
+    state: NestedLoopJoinStreamState,
 }
 
 impl NestedLoopJoinStream {
     fn poll_next_inner(
         &mut self,
-        _cx: &mut std::task::Context<'_>,
+        cx: &mut std::task::Context<'_>,
     ) -> Poll<Option<Result<RecordBatch>>> {
-        todo!()
+        use NestedLoopJoinStreamState::*;
+
+        loop {
+            return match self.state {
+                WaitBuildSide => {
+                    handle_stream_result!(ready!(self.collect_build_side(cx)))
+                }
+                FetchProbeBatch => {
+                    handle_stream_result!(ready!(self.fetch_probe_batch(cx)))
+                }
+                ProcessProbeBatch(_) => {
+                    handle_stream_result!(self.process_probe_batch())
+                }
+                ExhaustedProbeSide => {
+                    handle_stream_result!(self.process_unmatched_build_batch())
+                }
+                Completed => Poll::Ready(None),
+            };
+        }
+    }
+
+    fn collect_build_side(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Result<StreamResultState<Option<RecordBatch>>>> {
+        let inner_table = match self.inner_table.try_as_init_mut()?.poll_unpin(cx) {
+            Poll::Ready(data) => data?,
+            Poll::Pending => return Poll::Pending,
+        };
+
+        self.state = NestedLoopJoinStreamState::FetchProbeBatch;
+        self.inner_table = InnerTableState::Ready(inner_table);
+
+        Poll::Ready(Ok(StreamResultState::Continue))
+    }
+
+    fn fetch_probe_batch(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Result<StreamResultState<Option<RecordBatch>>>> {
+        match ready!(self.outer_table.poll_next_unpin(cx)) {
+            None => self.state = NestedLoopJoinStreamState::ExhaustedProbeSide,
+            Some(Ok(batch)) => self.state = NestedLoopJoinStreamState::ProcessProbeBatch(batch),
+            Some(Err(e)) => return Poll::Ready(Err(e)),
+        };
+        Poll::Ready(Ok(StreamResultState::Continue))
+    }
+
+    fn process_probe_batch(&mut self) -> Result<StreamResultState<Option<RecordBatch>>> {
+        let inner_table = self.inner_table.try_as_ready_mut()?;
+        let build_batch = &inner_table.batch;
+        let probe_batch = self.state.try_as_process_probe_batch()?;
+        let visited = &mut inner_table.visited;
+
+        let result = Self::join_build_probe_batch(
+            build_batch,
+            probe_batch,
+            &self.join_type,
+            &self.filter,
+            &self.column_indices,
+            self.schema.clone(),
+            visited,
+        )?;
+
+        self.state = NestedLoopJoinStreamState::FetchProbeBatch;
+
+        Ok(StreamResultState::Ready(Some(result)))
+    }
+
+    fn join_build_probe_batch(
+        build_batch: &RecordBatch,
+        probe_batch: &RecordBatch,
+        join_type: &JoinType,
+        filter: &Option<JoinFilter>,
+        column_indices: &[JoinColumnIndex],
+        schema: SchemaRef,
+        visited: &mut BooleanBufferBuilder,
+    ) -> Result<RecordBatch> {
+        let join_indices = (0..build_batch.num_rows())
+            .map(|build_idx| {
+                let probe_row_count = probe_batch.num_rows();
+                let build_indices = UInt64Array::from(vec![build_idx as u64; probe_row_count]);
+                let probe_indices = UInt32Array::from_iter_values(0..(probe_row_count as u32));
+                apply_join_filter(
+                    filter,
+                    build_indices,
+                    probe_indices,
+                    build_batch,
+                    probe_batch,
+                    JoinSide::Left,
+                )
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        let mut build_indices_builder = UInt64Builder::new();
+        let mut probe_indices_builder = UInt32Builder::new();
+        for (build_indices, probe_indices) in join_indices {
+            build_indices_builder
+                .append_values(build_indices.values(), &vec![true; build_indices.len()]);
+            probe_indices_builder
+                .append_values(probe_indices.values(), &vec![true; probe_indices.len()]);
+        }
+        let build_indices = build_indices_builder.finish();
+        let probe_indices = probe_indices_builder.finish();
+
+        // adjust indices according to join_type
+        let (build_indices, probe_indices) = match join_type {
+            JoinType::Inner | JoinType::Left => (build_indices, probe_indices),
+        };
+
+        build_indices
+            .iter()
+            .flatten()
+            .for_each(|idx| visited.set_bit(idx as usize, true));
+
+        build_batch_from_indices(
+            schema,
+            build_batch,
+            probe_batch,
+            &build_indices,
+            &probe_indices,
+            column_indices,
+            JoinSide::Left,
+        )
+    }
+
+    fn process_unmatched_build_batch(&mut self) -> Result<StreamResultState<Option<RecordBatch>>> {
+        if !matches!(self.join_type, JoinType::Left) {
+            self.state = NestedLoopJoinStreamState::Completed;
+            return Ok(StreamResultState::Continue);
+        }
+        let build_side = self.inner_table.try_as_ready_mut()?;
+        let build_batch = &build_side.batch;
+        let build_size = build_side.visited.len();
+
+        let build_indices_unmatched = (0..build_size)
+            .filter_map(|idx| (!build_side.visited.get_bit(idx)).then_some(idx as u64))
+            .collect::<UInt64Array>();
+
+        let mut builder = UInt32Builder::with_capacity(build_indices_unmatched.len());
+        builder.append_nulls(build_indices_unmatched.len());
+        let probe_indices = builder.finish();
+        let probe_batch = RecordBatch::new_empty(self.outer_table_schema.clone());
+
+        let result = build_batch_from_indices(
+            self.schema.clone(),
+            build_batch,
+            &probe_batch,
+            &build_indices_unmatched,
+            &probe_indices,
+            &self.column_indices,
+            JoinSide::Left,
+        )?;
+
+        self.state = NestedLoopJoinStreamState::Completed;
+
+        Ok(StreamResultState::Ready(Some(result)))
     }
 }
 
@@ -211,4 +399,140 @@ impl Stream for NestedLoopJoinStream {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::sync::Arc;
+
+    use arrow::compute::concat_batches;
+    use arrow_schema::{DataType, Field, Schema};
+    use futures::StreamExt;
+
+    use crate::{
+        expression::{
+            operator::Operator,
+            physical::{binary::BinaryExpr, column::ColumnExpr, literal::LiteralExpr},
+            values::ScalarValue,
+        },
+        io::reader::csv::options::CsvFileOpenerConfig,
+        plan::{
+            logical::join::JoinType,
+            physical::{
+                joins::utils::{JoinColumnIndex, JoinFilter, JoinSide},
+                plan::ExecutionPlan,
+                scan::csv::CsvExec,
+            },
+        },
+    };
+
+    use super::NestedLoopJoinExec;
+
+    fn create_nested_loop_join(
+        join_type: JoinType,
+        filter: Option<JoinFilter>,
+    ) -> NestedLoopJoinExec {
+        let left_schema = Arc::new(Schema::new(vec![
+            Field::new("l1", DataType::Utf8, true),
+            Field::new("l2", DataType::Int64, true),
+            Field::new("l3", DataType::Int64, true),
+        ]));
+        let lhs = CsvExec::new(
+            "testdata/csv/join_left.csv",
+            CsvFileOpenerConfig::new(left_schema),
+        );
+        let right_schema = Arc::new(Schema::new(vec![
+            Field::new("r1", DataType::Utf8, true),
+            Field::new("r2", DataType::Int64, true),
+            Field::new("r3", DataType::Int64, true),
+        ]));
+        let rhs = CsvExec::new(
+            "testdata/csv/join_right.csv",
+            CsvFileOpenerConfig::new(right_schema),
+        );
+        NestedLoopJoinExec::try_new(Arc::new(lhs), Arc::new(rhs), filter, join_type).unwrap()
+    }
+
+    #[tokio::test]
+    async fn test_nested_loop_join_left_with_filters() {
+        let intermediate_schema =
+            Arc::new(Schema::new(vec![Field::new("l2", DataType::Int64, true)]));
+        let column_indices = vec![JoinColumnIndex::new(1, JoinSide::Left)];
+        let expression = Arc::new(BinaryExpr::new(
+            Arc::new(ColumnExpr::new("l2", 0)),
+            Operator::NotEq,
+            Arc::new(LiteralExpr::new(ScalarValue::Int64(Some(1)))),
+        ));
+        let filter = JoinFilter::new(intermediate_schema, expression, column_indices);
+        let exec = create_nested_loop_join(JoinType::Left, Some(filter));
+        let schema = exec.schema();
+
+        let mut stream = exec.execute().unwrap();
+
+        let mut batches = Vec::new();
+        while let Some(Ok(batch)) = stream.next().await {
+            batches.push(batch);
+        }
+
+        let final_batch = concat_batches(&schema, batches.iter()).unwrap();
+        assert_eq!(final_batch.num_rows(), 16);
+        assert_eq!(final_batch.num_columns(), 6);
+    }
+
+    #[tokio::test]
+    async fn test_nested_loop_join_left_no_filters() {
+        let exec = create_nested_loop_join(JoinType::Left, None);
+        let schema = exec.schema();
+
+        let mut stream = exec.execute().unwrap();
+
+        let mut batches = Vec::new();
+        while let Some(Ok(batch)) = stream.next().await {
+            batches.push(batch);
+        }
+
+        let final_batch = concat_batches(&schema, batches.iter()).unwrap();
+        assert_eq!(final_batch.num_rows(), 18);
+        assert_eq!(final_batch.num_columns(), 6);
+    }
+
+    #[tokio::test]
+    async fn test_nested_loop_join_inner_with_filters() {
+        let intermediate_schema =
+            Arc::new(Schema::new(vec![Field::new("l2", DataType::Int64, true)]));
+        let column_indices = vec![JoinColumnIndex::new(1, JoinSide::Right)];
+        let expression = Arc::new(BinaryExpr::new(
+            Arc::new(ColumnExpr::new("r2", 0)),
+            Operator::NotEq,
+            Arc::new(LiteralExpr::new(ScalarValue::Int64(Some(100)))),
+        ));
+        let filter = JoinFilter::new(intermediate_schema, expression, column_indices);
+        let exec = create_nested_loop_join(JoinType::Inner, Some(filter));
+        let schema = exec.schema();
+
+        let mut stream = exec.execute().unwrap();
+
+        let mut batches = Vec::new();
+        while let Some(Ok(batch)) = stream.next().await {
+            batches.push(batch);
+        }
+
+        let final_batch = concat_batches(&schema, batches.iter()).unwrap();
+        assert_eq!(final_batch.num_rows(), 12);
+        assert_eq!(final_batch.num_columns(), 6);
+    }
+
+    #[tokio::test]
+    async fn test_nested_loop_join_inner_no_filters() {
+        let exec = create_nested_loop_join(JoinType::Inner, None);
+        let schema = exec.schema();
+
+        let mut stream = exec.execute().unwrap();
+
+        let mut batches = Vec::new();
+        while let Some(Ok(batch)) = stream.next().await {
+            batches.push(batch);
+        }
+
+        let final_batch = concat_batches(&schema, batches.iter()).unwrap();
+        assert_eq!(final_batch.num_rows(), 18);
+        assert_eq!(final_batch.num_columns(), 6);
+    }
+}
