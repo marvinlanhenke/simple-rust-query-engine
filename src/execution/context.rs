e3cff60fd37c39ae47ac50816ae149ac49621f18
marvinlanhenke <lanhenke@googlemail.com>
2024-08-29 08:00:27 +0200

    chore: add test 'register_csv'

--- src/execution/context.rs
+++ src/execution/context.rs
@@ -12,7 +12,7 @@ use crate::{
 use super::dataframe::DataFrame;
 
 /// Represents a [`SessionContext`] for managing query execution.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct SessionContext {
     table: Option<ListingTable>,
 }
@@ -21,6 +21,11 @@ impl SessionContext {
     /// Creates a new [`SessionContext`] instance.
     pub fn new() -> Self {
         Self { table: None }
+    }
+
+    /// Retrieves the current registered `ListingTable`.
+    pub fn table(&self) -> Option<&ListingTable> {
+        self.table.as_ref()
     }
 
     /// Reads a CSV file and creates a `DataFrame`.
@@ -32,15 +37,33 @@ impl SessionContext {
         Ok(DataFrame::new(plan))
     }
 
-    pub fn register_csv(&mut self, name: &str, path: &str, options: CsvReadOptions) -> Result<()> {
+    /// Registers a CSV `ListingTable` with the current `SessionContext`.
+    pub fn register_csv(&self, name: &str, path: &str, options: CsvReadOptions) -> Result<Self> {
         let resolved_schema = match options.schema() {
             Some(schema) => schema,
             None => CsvDataSource::infer_schema(path, &options)?,
         };
-        let source = Arc::new(CsvDataSource::try_new(path, options)?);
-        let table = ListingTable::new(name, path, resolved_schema, source);
-        self.table = Some(table);
+        let table = Some(ListingTable::new(name, path, resolved_schema));
 
-        Ok(())
+        Ok(Self { table })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::io::{reader::csv::options::CsvReadOptions, DataSource};
+
+    use super::SessionContext;
+
+    #[test]
+    fn test_context_register_csv() {
+        let ctx = SessionContext::new();
+        let ctx = ctx
+            .register_csv("simple", "testdata/csv/simple.csv", CsvReadOptions::new())
+            .unwrap();
+        let table = ctx.table().unwrap();
+
+        assert_eq!(table.name(), "simple");
+        assert_eq!(table.schema().fields().len(), 3);
     }
 }
