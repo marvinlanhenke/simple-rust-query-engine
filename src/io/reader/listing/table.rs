b253eabcfcd74c2d5fa8c8e32e59edbe2c1f83e9
marvinlanhenke <lanhenke@googlemail.com>
2024-08-29 08:06:33 +0200

    feat: impl ListingTable with scan

--- src/io/reader/listing/table.rs
+++ src/io/reader/listing/table.rs
@@ -2,28 +2,25 @@ use std::sync::Arc;
 
 use arrow_schema::SchemaRef;
 
-use crate::{error::Result, io::DataSource, plan::physical::plan::ExecutionPlan};
+use crate::{
+    error::Result,
+    io::{reader::csv::options::CsvFileOpenerConfig, DataSource},
+    plan::physical::{plan::ExecutionPlan, scan::csv::CsvExec},
+};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ListingTable {
     name: String,
     path: String,
     schema: SchemaRef,
-    source: Arc<dyn DataSource>,
 }
 
 impl ListingTable {
-    pub fn new(
-        name: impl Into<String>,
-        path: impl Into<String>,
-        schema: SchemaRef,
-        source: Arc<dyn DataSource>,
-    ) -> Self {
+    pub fn new(name: impl Into<String>, path: impl Into<String>, schema: SchemaRef) -> Self {
         Self {
             name: name.into(),
             path: path.into(),
             schema,
-            source,
         }
     }
 
@@ -34,10 +31,6 @@ impl ListingTable {
     pub fn path(&self) -> &str {
         &self.path
     }
-
-    pub fn source(&self) -> &dyn DataSource {
-        self.source.as_ref()
-    }
 }
 
 impl DataSource for ListingTable {
@@ -45,7 +38,57 @@ impl DataSource for ListingTable {
         self.schema.clone()
     }
 
-    fn scan(&self, _projection: Option<&Vec<String>>) -> Result<Arc<dyn ExecutionPlan>> {
-        todo!()
+    fn scan(&self, projection: Option<&Vec<String>>) -> Result<Arc<dyn ExecutionPlan>> {
+        let projection_idx = projection.map(|proj| {
+            proj.iter()
+                .filter_map(|name| self.schema.column_with_name(name).map(|(idx, _)| idx))
+                .collect::<Vec<_>>()
+        });
+        let config = CsvFileOpenerConfig::builder(self.schema.clone())
+            .with_projection(projection_idx)
+            .build();
+        let exec = CsvExec::new(&self.path, config);
+
+        Ok(Arc::new(exec))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use futures::StreamExt;
+
+    use crate::{
+        io::{reader::listing::table::ListingTable, DataSource},
+        tests::create_schema,
+    };
+
+    #[tokio::test]
+    async fn test_listing_table_scan_with_projection() {
+        let schema = create_schema();
+        let table = ListingTable::new("simple", "testdata/csv/simple.csv", Arc::new(schema));
+        let exec = table.scan(Some(&vec!["c1".to_string()])).unwrap();
+
+        let mut stream = exec.execute().unwrap();
+
+        while let Some(Ok(batch)) = stream.next().await {
+            assert_eq!(batch.num_rows(), 6);
+            assert_eq!(batch.num_columns(), 1);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_listing_table_scan_no_projection() {
+        let schema = create_schema();
+        let table = ListingTable::new("simple", "testdata/csv/simple.csv", Arc::new(schema));
+        let exec = table.scan(None).unwrap();
+
+        let mut stream = exec.execute().unwrap();
+
+        while let Some(Ok(batch)) = stream.next().await {
+            assert_eq!(batch.num_rows(), 6);
+            assert_eq!(batch.num_columns(), 3);
+        }
     }
 }
